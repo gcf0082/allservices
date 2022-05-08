4b54b4c7ecc931daee786aa3282260813dee0564
gcf0082 <13510989573@sina.cn>
2022-05-08 23:19:06 +0800

    将__import__换成importlib

--- agent/core/disptch_url.py
+++ agent/core/disptch_url.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import importlib 
 from core.plugin_manage import get_plugin_meta
 
 def dispatch(serviceName, url, context=None):
@@ -9,7 +10,9 @@ def dispatch(serviceName, url, context=None):
     for api in restapis['restapi']:
         if url == api['url']:
             sys.path.append(plugins_root + '/' + serviceName)
-            callit = getattr(__import__(api['endpoint']), api['function'])
+            #如果想每次都重新加载模块使用 importlib.reload
+            #callit = getattr(importlib.reload(importlib.import_module(api['endpoint'])), api['function']) 
+            callit = getattr(importlib.import_module(api['endpoint']), api['function'])            
             content = callit()
             print(content)
     return content
