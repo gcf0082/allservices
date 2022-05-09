e747d0c635103c8a22016bb6b846733e003f8ed3
gcf0082 <13510989573@sina.cn>
2022-05-09 23:05:02 +0800

    通过amis做了从后台获取数据构造前端树结构的例子

--- ui/ui.py
+++ ui/ui.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__,
             static_url_path='',
@@ -10,6 +10,41 @@ app = Flask(__name__,
 def root():
     return app.send_static_file('index.html')
 
+@app.route('/getdata')
+def getdata():
+    return jsonify({
+        "status":0,
+        "msg":"成功",
+         "data": {
+          "options": [
+            {
+              "label": "Project1",
+              "value": 1,
+              "children": [
+                {
+                  "label": "agent1",
+                  "value": 2
+                },
+                {
+                  "label": "agent5",
+                  "value": 3
+                }
+              ]
+            },
+            {
+              "label": "Project2",
+              "value": 4,
+              "children": [
+                {
+                  "label": "agent2",
+                  "value": 5
+                }
+              ]
+            }
+          ]
+        }
+
+    })
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=2000, debug=True)
